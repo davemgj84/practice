b375c24050443c1a0959386cfd62a6a8fa3e5c25
David <davemgj@gmail.com>
2021-03-15 17:05:09 -0700

    refactor kata3.js

--- kata3.js
+++ kata3.js
@@ -3,15 +3,13 @@
 const numberOfVowels = function (data) {
   let vowels = 0;
   for (let i = 0; i < data.length; i++) {
-    if (data[i] === "a") {
-      vowels += 1;
-    } else if (data[i] === "e") {
-      vowels += 1;
-    } else if (data[i] === "i") {
-      vowels += 1;
-    } else if (data[i] === "o") {
-      vowels += 1;
-    } else if (data[i] === "u") {
+    if (
+      data[i] === "a" ||
+      data[i] === "e" ||
+      data[i] === "i" ||
+      data[i] === "o" ||
+      data[i] === "u"
+    ) {
       vowels += 1;
     }
   }
@@ -21,5 +19,3 @@ const numberOfVowels = function (data) {
 console.log(numberOfVowels("orange"));
 console.log(numberOfVowels("lighthouse labs"));
 console.log(numberOfVowels("aeiou"));
-
-// I am sure there is a way to tighten up this code, but I found it works quite well :)
