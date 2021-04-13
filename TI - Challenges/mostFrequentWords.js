b730507987ff2db14b5620e2a9663fe2fd7cdfa4
David <davemgj@gmail.com>
2021-04-13 13:11:25 -0700

    refactored

--- TI - Challenges/mostFrequentWords.js
+++ TI - Challenges/mostFrequentWords.js
@@ -20,20 +20,11 @@
 */
 
 const mostFrequentWords = (array, k) => {
-  // input check for k - needs to be equal to or less than array length
   if (k > array.length) {
     return "Please check inputs - second input needs to be equal to or less than array length";
   }
-
-  // create an object that will hold the words(keys) and frequency they occur (values)
-  const object = {};
-
-  // create an empty array that will hold the results
   const results = [];
-
-  // map through the array, to create key/value pairs in our object above.
-  // if the key(word) already exists within the object, increment the value,
-  // if it does not, create the key/value pair and set initial value to 1.
+  const object = {};
   array.map((word) => {
     if (object[word]) {
       object[word]++;
@@ -41,16 +32,9 @@ const mostFrequentWords = (array, k) => {
       object[word] = 1;
     }
   });
-
-  // Create a new array from the object. Each element in the array will be a
-  // key/value pair from the object. They will be sorted based on the values,
-  // if two values are the same, they will be sorted alphabetically.
-  // Reversed so they are highest to lowest
   const newArray = Object.entries(object)
     .sort((a, b) => (a[1] !== b[1] ? a[1] - b[1] : b[0] < a[0] ? -1 : 1))
     .reverse();
-
-  // loop through the new array, based on "k", and push the key(word) to the results array.
   for (let i = 0; i < k; i++) {
     results.push(newArray[i][0]);
   }
